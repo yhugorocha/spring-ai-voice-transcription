adb0df47b85795cdcc6891dd3f747eab6acc9e47
Hugo Silva <yhugorocha@gmail.com>
2025-05-19 18:37:54 -0300

    initial commit

---
+++ src/main/java/io/github/yhugorocha/SpringAiVoiceTranscriptionApplication.java
@@ -0,0 +1,13 @@
+package io.github.yhugorocha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiVoiceTranscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiVoiceTranscriptionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/io/github/yhugorocha/config/WebConfig.java
@@ -0,0 +1,25 @@
+package io.github.yhugorocha.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080/", "http://localhost:3000/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
